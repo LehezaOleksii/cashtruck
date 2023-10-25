824708da2f0c1219f76160b4084a2f38535b874e
Leheza Oleksii <oleksii.leheza@gmail.com>
2023-10-25 09:36:49 +0300

    change repositories

--- src/main/java/com/projects/oleksii/leheza/cashtruck/controllers/ClientController.java
+++ src/main/java/com/projects/oleksii/leheza/cashtruck/controllers/ClientController.java
@@ -12,38 +12,38 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.projects.oleksii.leheza.cashtruck.entity.Client;
-import com.projects.oleksii.leheza.cashtruck.services.ClientService;
+import com.projects.oleksii.leheza.cashtruck.domain.Client;
+import com.projects.oleksii.leheza.cashtruck.service.ClientService;
 
 @RestController
 @RequestMapping(path = "/client")
 public class ClientController {
 
-	@Autowired
-	private ClientService clientService;
+    @Autowired
+    private ClientService clientService;
 
-	@GetMapping(path = "/all")
-	public List<Client> showAllClients() {
-		return clientService.findAll();
-	}
+    @GetMapping(path = "/all")
+    public List<Client> showAllClients() {
+        return clientService.findAll();
+    }
 
-	@GetMapping(path = "/save")
-	public void RegisterNewClient(@RequestBody Client client) {
-		clientService.addNewClient(client);
-	}
+    @GetMapping(path = "/save")
+    public void registerNewClient(@RequestBody Client client) {
+        clientService.saveClient(client);
+    }
 
-	@GetMapping(path = "/{clientId}")
-	public void showClient(@PathVariable(value = "clientId") Long clientId) {
+    @GetMapping(path = "/{clientId}")
+    public void showClient(@PathVariable(value = "clientId") Long clientId) {
+        clientService.getClient(clientId);
+    }
 
-	}
+    @DeleteMapping(path = "delete/{clientId}")
+    public void deletClientById(@PathVariable(value = "clientId") Long clientId) {
+        clientService.deleteById(clientId);
+    }
 
-	@DeleteMapping(path = "{clientId}")
-	public void deletClientById(@PathVariable(value = "clientId") Long clientId) {
-		clientService.deleteClientById(clientId);
-	}
-
-	@PutMapping(path = { "clientId" })
-	public void updateStudent(@PathVariable("clientId") Long clientId, @ModelAttribute("client") Client client) {
-		clientService.updateClient(clientId, client);
-	}
+    @PutMapping(path = {"update/{clientId}"})
+    public void updateStudent(@PathVariable("clientId") Long clientId, @ModelAttribute("client") Client client) {
+        clientService.updateClient(clientId, client);
+    }
 }
--- src/main/java/com/projects/oleksii/leheza/cashtruck/repository/ClientRepository.java
+++ src/main/java/com/projects/oleksii/leheza/cashtruck/repository/ClientRepository.java
@@ -3,14 +3,13 @@ package com.projects.oleksii.leheza.cashtruck.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.projects.oleksii.leheza.cashtruck.entity.Client;
+import com.projects.oleksii.leheza.cashtruck.domain.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
-	@Query ("SELECT c FROM client c where c.email = ?1")
-	Optional<Client> findClientByEmail(String email);
+    //	@Query ("SELECT c FROM client c where c.email = ?1")
+    Client findByEmail(String email); // Optional<Client>
 }
