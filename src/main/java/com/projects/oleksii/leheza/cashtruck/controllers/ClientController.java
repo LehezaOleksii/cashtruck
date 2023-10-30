20a73b3110e32c8827ea76a25a1d98771e33f6da
Leheza Oleksii <oleksii.leheza@gmail.com>
2023-10-30 12:27:19 +0200

    add CRUD methods to client controller

--- src/main/java/com/projects/oleksii/leheza/cashtruck/controllers/ClientController.java
+++ src/main/java/com/projects/oleksii/leheza/cashtruck/controllers/ClientController.java
@@ -1,19 +1,13 @@
 package com.projects.oleksii.leheza.cashtruck.controllers;
 
-import java.util.List;
-
+import com.projects.oleksii.leheza.cashtruck.dto.ClientDto;
+import com.projects.oleksii.leheza.cashtruck.service.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.projects.oleksii.leheza.cashtruck.domain.Client;
 import com.projects.oleksii.leheza.cashtruck.service.ClientService;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping(path = "/client")
@@ -23,27 +17,66 @@ public class ClientController {
     private ClientService clientService;
 
     @GetMapping(path = "/all")
-    public List<Client> showAllClients() {
-        return clientService.findAll();
+    public ModelAndView showAllClients() {
+        ModelAndView modelAndView = new ModelAndView("client/all");//TODO first 20 than next 20 (front)
+        modelAndView.addObject("clients", clientService.findAll());
+        return modelAndView;
     }
 
-    @GetMapping(path = "/save")
-    public void registerNewClient(@RequestBody Client client) {
-        clientService.saveClient(client);
+    @PostMapping(path = "/login")
+    public ModelAndView registerNewClient(@RequestBody ClientDto clientDto) {
+        ModelAndView modelAndView;
+        if (clientService.findByEmail(clientDto.getEmail()) != null) {
+            modelAndView = new ModelAndView("redirect:/client/login");
+            modelAndView.addObject("client", clientDto);
+            //TODO error email is already taken
+            return modelAndView;
+        }
+        clientService.saveClient(clientDto);
+//        modelAndView = new ModelAndView("client/dashboard/{clientId}");
+//        modelAndView.addObject("clientId",clientService.findByEmail(clientEmail).getId());
+        modelAndView = new ModelAndView("client/dashboard/{clientId}");
+        return modelAndView;
     }
 
-    @GetMapping(path = "/{clientId}")
-    public void showClient(@PathVariable(value = "clientId") Long clientId) {
-        clientService.getClient(clientId);
+    @GetMapping(path = "/inforamtion/{clientId}")
+    public ModelAndView showClientInfo(@PathVariable(value = "clientId") Long clientId) {
+        ModelAndView modelAndView = new ModelAndView("client/information");
+        Client client = clientService.getClient(clientId);
+//        modelAndView.addObject("clientId", client.getId());
+        ClientDto clientDto = ClientDto.builder()
+                .firstname(client.getFirstname())
+                .lastname(client.getLastname())
+                .email(client.getEmail())
+                .password(client.getPassword())
+                .build();
+        modelAndView.addObject("client", clientDto);
+        return modelAndView;
     }
 
-    @DeleteMapping(path = "delete/{clientId}")
-    public void deletClientById(@PathVariable(value = "clientId") Long clientId) {
+    @GetMapping(path = "/dashboard/{clientId}")
+    public ModelAndView showClientDashboard(@PathVariable(value = "clientId") Long clientId) {
+        ModelAndView modelAndView = new ModelAndView("client/dashboard");
+        Client client = clientService.getClient(clientId);
+//        modelAndView.addObject("clientId", client.getId());
+        modelAndView.addObject("client", client);
+        return modelAndView;
+    }
+
+    @PutMapping(path = {"/update/{clientId}"})
+    public ModelAndView updateClientInfo(@PathVariable("clientId") Long clientId, @ModelAttribute("client") ClientDto clientDto) {
+        try {
+            clientService.updateClientInfo(clientId, clientDto);
+            System.out.println("UPDATE client");
+        } catch (Exception e) {
+            return new ModelAndView("client/dashboard/{clientId}");
+        }
+        return new ModelAndView("redirect:/client/dashboard/{clientId}");
+    }
+
+    @DeleteMapping(path = "/delete/{clientId}")
+    public ModelAndView deleteClientById(@PathVariable(value = "clientId") Long clientId) {
         clientService.deleteById(clientId);
-    }
-
-    @PutMapping(path = {"update/{clientId}"})
-    public void updateStudent(@PathVariable("clientId") Long clientId, @ModelAttribute("client") Client client) {
-        clientService.updateClient(clientId, client);
+        return new ModelAndView("client/all");
     }
 }
--- src/main/java/com/projects/oleksii/leheza/cashtruck/service/ClientService.java
+++ src/main/java/com/projects/oleksii/leheza/cashtruck/service/ClientService.java
@@ -1,18 +1,20 @@
 package com.projects.oleksii.leheza.cashtruck.service;
 
-import java.util.List;
-import java.util.Optional;
-
 import com.projects.oleksii.leheza.cashtruck.domain.Client;
+import com.projects.oleksii.leheza.cashtruck.dto.ClientDto;
+
+import java.util.List;
 
 public interface ClientService {
 
-	public void saveClient(Client client);
+	public void saveClient(ClientDto clientDto);
 	public List<Client> findAll();
 
 	public Client findByEmail(String email);
 
 	public void deleteById(Long clientId);
+
+	public void updateClientInfo(Long clientId, ClientDto clientDto);
 
 	public void updateClient(Long clientId, Client client);
 
