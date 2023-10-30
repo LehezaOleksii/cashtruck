5a0302f1f025f8909e178505f8eb4c3d35abe4ec
Leheza Oleksii <oleksii.leheza@gmail.com>
2023-10-30 12:28:09 +0200

    Uncomment query for find by email

--- src/main/java/com/projects/oleksii/leheza/cashtruck/repository/ClientRepository.java
+++ src/main/java/com/projects/oleksii/leheza/cashtruck/repository/ClientRepository.java
@@ -3,6 +3,7 @@ package com.projects.oleksii.leheza.cashtruck.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.projects.oleksii.leheza.cashtruck.domain.Client;
@@ -10,6 +11,6 @@ import com.projects.oleksii.leheza.cashtruck.domain.Client;
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
-    //	@Query ("SELECT c FROM client c where c.email = ?1")
+    	@Query("SELECT c FROM Client c where c.email = ?1")
     Client findByEmail(String email); // Optional<Client>
 }
