d45ee77116e8ba013f79a5b77963e41d82be6173
Leheza Oleksii <oleksii.leheza@gmail.com>
2023-10-31 10:09:24 +0200

    Create client DTO class

---
+++ src/main/java/com/projects/oleksii/leheza/cashtruck/dto/ClientDto.java
@@ -0,0 +1,28 @@
+package com.projects.oleksii.leheza.cashtruck.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class ClientDto {
+
+    @Size(max=50)
+    private String firstname;
+    @Size(max=50)
+    private String lastname;
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+    @NotEmpty
+    @NotBlank
+    private String email;
+    @NotEmpty
+    @NotBlank
+    @Size(min=7,max=50)
+    private String password;
+}
