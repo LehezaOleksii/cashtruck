89531a75dacd9dee265b0904d98535cbb2af8429
Leheza Oleksii <oleksii.leheza@gmail.com>
2023-11-20 14:38:31 +0200

    comment security config

--- src/main/java/com/projects/oleksii/leheza/cashtruck/config/JwtUtils.java
+++ src/main/java/com/projects/oleksii/leheza/cashtruck/config/JwtUtils.java
@@ -1,68 +1,68 @@
-package com.projects.oleksii.leheza.cashtruck.config;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
-
-@Component
-public class JwtUtils {
-
-    private final String jwtSigningKey = "jwtSigningKey";
-
-    public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);
-    }
-
-    public Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
-
-    public boolean hasClaim(String token, String claimName) {
-        final Claims claims = extractAllClaims(token);
-        return claims.get(claimName) != null;
-    }
-
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
-        final Claims claims = extractAllClaims(token);
-        return claimsResolvers.apply(claims);
-    }
-
-    public String generateToken(UserDetails userDetails) {
-        Map<String, Object> claims = new HashMap<>();
-        return createToken(claims, userDetails);
-    }
-
-    public Boolean isTokenValid(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return (!isTokenExpired(token) && username.equals(userDetails.getUsername()));
-    }
-
-    private String createToken(Map<String, Object> claims, UserDetails userDetails) {
-        return Jwts.builder().setClaims(claims)
-                .setSubject(userDetails.getUsername())
-                .claim("authorities", userDetails.getAuthorities())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(24)))
-                .signWith(SignatureAlgorithm.ES256, jwtSigningKey).compact();
-
-    }
-
-    private Claims extractAllClaims(String token) {
-        return Jwts.parser()
-                .setSigningKey(jwtSigningKey)
-                .parseClaimsJws(token)
-                .getBody();
-    }
-
-    private Boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-}
+//package com.projects.oleksii.leheza.cashtruck.config;
+//
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Date;
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.concurrent.TimeUnit;
+//import java.util.function.Function;
+//
+//@Component
+//public class JwtUtils {
+//
+//    private final String jwtSigningKey = "jwtSigningKey";
+//
+//    public String extractUsername(String token) {
+//        return extractClaim(token, Claims::getSubject);
+//    }
+//
+//    public Date extractExpiration(String token) {
+//        return extractClaim(token, Claims::getExpiration);
+//    }
+//
+//    public boolean hasClaim(String token, String claimName) {
+//        final Claims claims = extractAllClaims(token);
+//        return claims.get(claimName) != null;
+//    }
+//
+//    public <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
+//        final Claims claims = extractAllClaims(token);
+//        return claimsResolvers.apply(claims);
+//    }
+//
+//    public String generateToken(UserDetails userDetails) {
+//        Map<String, Object> claims = new HashMap<>();
+//        return createToken(claims, userDetails);
+//    }
+//
+//    public Boolean isTokenValid(String token, UserDetails userDetails) {
+//        final String username = extractUsername(token);
+//        return (!isTokenExpired(token) && username.equals(userDetails.getUsername()));
+//    }
+//
+//    private String createToken(Map<String, Object> claims, UserDetails userDetails) {
+//        return Jwts.builder().setClaims(claims)
+//                .setSubject(userDetails.getUsername())
+//                .claim("authorities", userDetails.getAuthorities())
+//                .setIssuedAt(new Date(System.currentTimeMillis()))
+//                .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(24)))
+//                .signWith(SignatureAlgorithm.ES256, jwtSigningKey).compact();
+//
+//    }
+//
+//    private Claims extractAllClaims(String token) {
+//        return Jwts.parser()
+//                .setSigningKey(jwtSigningKey)
+//                .parseClaimsJws(token)
+//                .getBody();
+//    }
+//
+//    private Boolean isTokenExpired(String token) {
+//        return extractExpiration(token).before(new Date());
+//    }
+//}
